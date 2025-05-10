94d941c2431196e8e196fc82438cce4af9662310
Gabriel Francisco Souza de Jesus <gabriellfsj@gmail.com>
2025-05-10 09:29:07 -0300

    Ajuste no texto do teste de acervo

--- cypress/e2e/acervo.cy.js
+++ cypress/e2e/acervo.cy.js
@@ -12,74 +12,74 @@ describe('Teste regressivo do site Aya Books', () => {
         })
 
 
-        it('Clicar filtro Administrção da seção Ebooks', () => {
+        it('Clicar no filtro Administrção da seção Ebooks', () => {
             acervo.selectfiltroAdministracaoBooks()
             acervo.validarSecaoAposFiltro('Administração')
 
         })
 
-        it('Clicar filtro Artes da seção Ebooks', () => {
+        it('Clicar no filtro Artes da seção Ebooks', () => {
             acervo.selectfiltroArtesBooks()
             acervo.validarSecaoAposFiltro('Artes')
 
         })
 
-        it('Clicar filtro Administrção da seção Minibooks', () => {
+        it('Clicar no filtro Administrção da seção Minibooks', () => {
             acervo.selectfiltroAdministracaoMbooks()
             acervo.validarSecaoAposFiltro('Administração')
 
         })
 
-        it('Clicar filtro Artes da seção Minibooks', () => {
+        it('Clicar no filtro Artes da seção Minibooks', () => {
             acervo.selectfiltroArtesMbooks()
             acervo.validarSecaoAposFiltro('Artes')
 
         })
 
-        it('Clicar filtro Administrção da seção Audiobooks', () => {
+        it('Clicar no filtro Administrção da seção Audiobooks', () => {
             acervo.selectfiltroAdministracaoAbooks()
             acervo.validarSecaoAposFiltro('Administração')
 
         })
 
-        it('Clicar filtro Artes da seção Audiobook', () => {
+        it('Clicar no filtro Artes da seção Audiobook', () => {
             acervo.selectfiltroArtesAbooks()
             acervo.validarSecaoAposFiltro('Artes')
 
         })
 
-        it('Clicar filtro Administrção da seção Audiobooks', () => {
+        it('Clicar no filtro Administrção da seção Audiobooks', () => {
             acervo.selectfiltroAdministracaoAbooks()
             acervo.validarSecaoAposFiltro('Administração')
 
         })
 
-        it('Clicar filtro Artes da seção Audiobook', () => {
+        it('Clicar no filtro Artes da seção Audiobook', () => {
             acervo.selectfiltroArtesAbooks()
             acervo.validarSecaoAposFiltro('Artes')
 
         })
 
-        it('Clicar filtro Administrção da seção Biblioteca Ebooks', () => {
+        it('Clicar no filtro Administrção da seção Biblioteca Ebooks', () => {
             acervo.selectfiltroAdministracaoBiblioBooks()
             acervo.validarSecaoAposFiltro('Administração')
 
         })
 
-        it('Clicar filtro Artes da seção Biblioteca Ebook', () => {
+        it('Clicar no filtro Artes da seção Biblioteca Ebook', () => {
             acervo.selectfiltroArtesBiblioBooks()
             acervo.validarSecaoAposFiltro('Artes')
 
         })
 
 
-        it('Clicar filtro Administrção da seção Biblioteca Audiobooks', () => {
+        it('Clicar no filtro Administrção da seção Biblioteca Audiobooks', () => {
             acervo.selectfiltroAdministracaoBiblioAbooks()
             acervo.validarSecaoAposFiltro('Administração')
 
         })
 
-        it('Clicar filtro Artes da seção Biblioteca Audiobooks', () => {
+        it('Clicar no filtro Artes da seção Biblioteca Audiobooks', () => {
             acervo.selectfiltroArtesBiblioAbooks()
             acervo.validarSecaoAposFiltro('Artes')
 
